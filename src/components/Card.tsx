35482296fd19344c1cadc3023bf96b0e958fc4ec
Dominik Sęk <d.sek464@gmail.com>
2025-07-20 21:22:50 +0200

    add loader for details image, change fallback for card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,49 +1,52 @@
 import { useState } from 'react';
 import { Link } from 'react-router';
 import type { PokemonBaseInfo } from '../types/pokemon';
+import { LoadingSpinner } from './LoadingSpinner';
 
 interface CardProps {
-	className?: string;
-	pokemon: PokemonBaseInfo;
-	prevNeighbor?: PokemonBaseInfo | null;
-	nextNeighbor?: PokemonBaseInfo | null;
+    className?: string;
+    pokemon: PokemonBaseInfo;
+    prevNeighbor?: PokemonBaseInfo | null;
+    nextNeighbor?: PokemonBaseInfo | null;
 }
 
 export const Card = (props: CardProps) => {
-	const [loaded, setLoaded] = useState(false);
+    const [loaded, setLoaded] = useState(false);
 
-	const pokemonImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.pokemon.id}.svg`;
-	const pokemonFallbackMissing = `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/0.png`;
+    const pokemonImageUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${props.pokemon.id}.svg`;
+    const pokemonFallbackMissing = `https://raw.githubusercontent.com/PokeAPI/sprites/refs/heads/master/sprites/pokemon/0.png`;
 
-	return (
-		<div
-			className={`relative h-full w-full min-w-36 max-w-72 max-h-48 bg-white/30 hover:bg-white/60 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg  border border-gray-100 ${props.className || ''}`}>
-			<Link to={`/pokemon/${props.pokemon.id}`}>
-				{!loaded && (
-					<div className=" absolute inset-0 m-6 rounded-md bg-gray-200 animate-pulse"></div>
-				)}
-				<object
-					data={pokemonImageUrl}
-					type="image/svg"
-					className="relative w-full h-32 p-6 object-contain cursor-pointer">
-					<img
-						onLoad={() => setLoaded(true)}
-						loading="lazy"
-						src={pokemonImageUrl}
-						alt={props.pokemon.name}
-						onError={(e) => {
-							e.currentTarget.src = pokemonFallbackMissing;
-							setLoaded(true);
-						}}
-						className="absolute top-0 left-0 w-full p-6 h-32 object-contain cursor-pointer"
-					/>
-				</object>
-				<div className="px-4 py-2">
-					<div className="font-medium text-xl mb-2 capitalize text-center">
-						{props.pokemon.name}
-					</div>
-				</div>
-			</Link>
-		</div>
-	);
+    return (
+        <div
+            className={`relative h-full w-full min-w-36 max-w-72 max-h-48 bg-white/30 hover:bg-white/60 rounded-md bg-clip-padding backdrop-filter backdrop-blur-lg  border border-gray-100 ${props.className || ''}`}>
+            <Link to={`/pokemon/${props.pokemon.id}`}>
+                {!loaded && (
+                    <div className="flex justify-center items-center w-full h-full mx-auto my-4 ">
+                        <LoadingSpinner />
+                    </div>
+                )}
+                <object
+                    data={pokemonImageUrl}
+                    type="image/svg"
+                    className="relative w-full h-32 p-6 object-contain cursor-pointer">
+                    <img
+                        onLoad={() => setLoaded(true)}
+                        loading="lazy"
+                        src={pokemonImageUrl}
+                        alt={props.pokemon.name}
+                        onError={(e) => {
+                            e.currentTarget.src = pokemonFallbackMissing;
+                            setLoaded(true);
+                        }}
+                        className="absolute top-0 left-0 w-full p-6 h-32 object-contain cursor-pointer"
+                    />
+                </object>
+                <div className="px-4 py-2">
+                    <div className="font-medium text-xl mb-2 capitalize text-center">
+                        {props.pokemon.name}
+                    </div>
+                </div>
+            </Link>
+        </div>
+    );
 };
--- src/components/PokemonDetails/FlavorImage.tsx
+++ src/components/PokemonDetails/FlavorImage.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import type { PokemonDetails } from '../../types/pokemon-details';
 import type { PokemonSpecies } from '../../types/pokemon-species';
 import { Blockquote } from '../Blockquote';
+import { LoadingSpinner } from '../LoadingSpinner';
 
 interface FlavorImageProps {
 	details: { details: PokemonDetails; species: PokemonSpecies };
@@ -9,7 +10,9 @@ interface FlavorImageProps {
 }
 
 export const FlavorImage = (props: FlavorImageProps) => {
-	const [flavorText, setFlavorText] = useState('');
+  const [flavorText, setFlavorText] = useState('');
+  const [imageLoaded, setImageLoaded] = useState(false)
+
 	useEffect(() => {
 		const flavorTextEntries = props.details.species.flavor_text_entries;
 		if (flavorTextEntries) {
@@ -27,16 +30,23 @@ export const FlavorImage = (props: FlavorImageProps) => {
 	}, []);
 
 	return (
-		<div>
+    <div>
+      {!imageLoaded && (
+        <div className="flex justify-center items-center w-64 h-64 mx-auto my-4 ">
+          <LoadingSpinner />
+        </div>
+      )}
 			<img
-				src={props.spriteUrl}
+        src={props.spriteUrl}
+        onLoad={()=>setImageLoaded(true)}
 				onError={(e) => {
 					e.currentTarget.src =
-						'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png';
+            'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/0.png';
+          setImageLoaded(true)
 				}}
 				loading="lazy"
 				alt={props.details.details.name}
-				className="w-64 h-64 object-contain antialiased mx-auto my-4"
+				className={`w-64 h-64 object-contain antialiased mx-auto my-4 transition-opacity duration-300 ${imageLoaded ? 'opacity-100' : 'opacity-0 absolute pointer-events-none'} `}
 				style={{ maxWidth: '100%', maxHeight: '400px' }}
 			/>
 
