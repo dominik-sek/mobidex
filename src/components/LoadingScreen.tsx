051505c44ab4ba909d6eb3c7c655f9503175a30e
Dominik Sęk <d.sek464@gmail.com>
2025-07-20 20:23:00 +0200

    fix for loading screen path

--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,7 +1,7 @@
 export const LoadingScreen = () => {
 	return (
 		<div className="flex flex-col items-center justify-center h-screen">
-			<img src="../loader.gif" alt="Loading..." className="w-24 h-24 " />
+			<img src="/loader.gif" alt="Loading..." className="w-24 h-24 " />
 			Loading...
 		</div>
 	);
